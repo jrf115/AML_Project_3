5ed0358f8778386fbed790fd4a7eb324df46b87e
jrf115 <jrf115@zips.uakron.edu>
2020-04-19 18:54:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AML_Project_3
+ The objective of this project is to understand how a multilayer perceptron neural network works.
---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+Applied Machine Learning Project_3: Using Neural Networks
+John Fahringer
+This program tries to build a feedforward neural network (multilayer perceptron)
+based on the Wisconsin breast cancer dataset.
+"""
