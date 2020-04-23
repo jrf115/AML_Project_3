35603789451e7a2277eaa759e3f2b0c1dcd0d6f3
jrf115 <jrf115@zips.uakron.edu>
2020-04-22 21:44:08 -0400

    Fix data

---
+++ RegionPlot.py
@@ -0,0 +1,99 @@
+# from somewhere
+# with modification
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib.colors import ListedColormap
+
+
+def plot_class_regions_for_classifier(clf, X, y, X_test=None, y_test=None, title=None, target_names = None, plot_decision_regions = True):
+    numClasses = np.amax(y) + 1
+    color_list_light = ['#FFFFAA', '#EFEFEF', '#AAFFAA', '#AAAAFF']
+    color_list_bold = ['#EEEE00', '#000000', '#00CC00', '#0000CC']
+    cmap_light = ListedColormap(color_list_light[0:numClasses])
+    cmap_bold = ListedColormap(color_list_bold[0:numClasses])
+
+    h = 0.03
+    k = 0.5
+    x_plot_adjust = 0.1
+    y_plot_adjust = 0.1
+    plot_symbol_size = 50
+
+    x_min = X.iloc[:, 0].min()
+    x_max = X.iloc[:, 0].max()
+    y_min = X.iloc[:, 1].min()
+    y_max = X.iloc[:, 1].max()
+    x2, y2 = np.meshgrid(np.arange(x_min-k, x_max+k, h), np.arange(y_min-k, y_max+k, h))
+
+    P = clf.predict(np.c_[x2.ravel(), y2.ravel()])
+    P = P.reshape(x2.shape)
+    plt.figure()
+    if plot_decision_regions:
+        plt.contourf(x2, y2, P, cmap=cmap_light, alpha = 0.8)
+
+    plt.scatter(X.iloc[:, 0], X.iloc[:, 1], c=y, cmap=cmap_bold, s=plot_symbol_size, edgecolor = 'black')
+    plt.xlim(x_min - x_plot_adjust, x_max + x_plot_adjust)
+    plt.ylim(y_min - y_plot_adjust, y_max + y_plot_adjust)
+
+    if (X_test is not None):
+        plt.scatter(X_test.iloc[:, 0], X_test.iloc[:, 1], c=y_test, cmap=cmap_bold, s=plot_symbol_size, marker='^', edgecolor = 'black')
+        train_score = clf.score(X, y)
+        test_score = clf.score(X_test, y_test)
+        title = title + "\nTrain score = {:.2f}, Test score = {:.2f}".format(train_score, test_score)
+
+    if (target_names is not None):
+        legend_handles = []
+        for i in range(0, len(target_names)):
+            patch = mpatches.Patch(color=color_list_bold[i], label=target_names[i])
+            legend_handles.append(patch)
+        plt.legend(loc=0, handles=legend_handles)
+
+    if (title is not None):
+        plt.title(title)
+    plt.show()
+
+
+def plot_class_regions_for_classifier_subplot(clf, X, y, X_test, y_test, title, subplot, target_names = None, plot_decision_regions = True):
+    numClasses = np.amax(y) + 1
+    color_list_light = ['#FFFFAA', '#EFEFEF', '#AAFFAA', '#AAAAFF']
+    color_list_bold = ['#EEEE00', '#000000', '#00CC00', '#0000CC']
+    cmap_light = ListedColormap(color_list_light[0:numClasses])
+    cmap_bold  = ListedColormap(color_list_bold[0:numClasses])
+
+    h = 0.03
+    k = 0.5
+    x_plot_adjust = 0.1
+    y_plot_adjust = 0.1
+    plot_symbol_size = 50
+
+    x_min = X.iloc[:, 0].min()
+    x_max = X.iloc[:, 0].max()
+    y_min = X.iloc[:, 1].min()
+    y_max = X.iloc[:, 1].max()
+    x2, y2 = np.meshgrid(np.arange(x_min - k, x_max + k, h), np.arange(y_min - k, y_max + k, h))
+    P = clf.predict(np.c_[x2.ravel(), y2.ravel()])
+    P = P.reshape(x2.shape)
+
+    if plot_decision_regions:
+        subplot.contourf(x2, y2, P, cmap=cmap_light, alpha = 0.8)
+
+    subplot.scatter(X.iloc[:, 0], X.iloc[:, 1], c=y, cmap=cmap_bold, s=plot_symbol_size, edgecolor = 'black')
+    subplot.set_xlim(x_min - x_plot_adjust, x_max + x_plot_adjust)
+    subplot.set_ylim(y_min - y_plot_adjust, y_max + y_plot_adjust)
+
+    if (X_test is not None):
+        subplot.scatter(X_test.iloc[:, 0], X_test.iloc[:, 1], c=y_test, cmap=cmap_bold, s=plot_symbol_size, marker='^', edgecolor = 'black')
+        train_score = clf.score(X, y)
+        test_score  = clf.score(X_test, y_test)
+        title = title + "\nTrain score = {:.2f}, Test score = {:.2f}".format(train_score, test_score)
+
+    subplot.set_title(title)
+
+    if (target_names is not None):
+        legend_handles = []
+        for i in range(0, len(target_names)):
+            patch = mpatches.Patch(color=color_list_bold[i], label=target_names[i])
+            legend_handles.append(patch)
+        subplot.legend(loc=0, handles=legend_handles)
+
--- main.py
+++ main.py
@@ -4,3 +4,22 @@ John Fahringer
 This program tries to build a feedforward neural network (multilayer perceptron)
 based on the Wisconsin breast cancer dataset.
 """
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import RegionPlot as rp
+
+wisconsin = pd.read_csv('data/wdbc.data', ',')
+X = wisconsin[[
+'Mean_Radius','Mean_Texture','Mean_Perimeter','Mean_Area','Mean_Smoothness','Mean_Compactness','Mean_Concavity','Mean_Concave_Points','Mean_Symmetry','Mean_Fractal_Dimension',
+'Radius','Texture','Perimeter','Area','Smoothness','Compactness','Concavity','Concave_Points','Symmetry','Fractal_Dimension',
+'Worst_Radius','Worst_Texture','Worst_Perimeter','Worst_Area','Worst_Smoothness','Worst_Compactness','Worst_Concavity','Worst_Concave_Points','Worst_Symmetry','Worst_Fractal_Dimension'
+]]
+y = wisconsin['Diagnosis']
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+
+print(wisconsin.shape)
+print("Wisconsin data: \n" , X)
+print("Wisconsin target: \n" , y)
